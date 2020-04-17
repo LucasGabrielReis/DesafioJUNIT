1cacbc765beb30951a6fb1b7eaf41e48b50d543c
Lucas Gabriel <lg.carlos@ymail.com>
2020-04-17 20:26:58 -0300

    Condicionais e valores das variaveis corrigidos na classe e metodos para testar cada resultado com Junit criados

--- ProjetoNac/src/JulgamentoPrisioneiro.java
+++ ProjetoNac/src/JulgamentoPrisioneiro.java
@@ -1,21 +1,22 @@
 public class JulgamentoPrisioneiro {
-	private int PENA_INOCENCIA = 10;
-	private int PENA_CONDENACAO_MUTUA = 15;
+	private int PENA_INOCENCIA = 0;
+	private int PENA_CONDENACAO_MUTUA = 5;
 	private int PENA_CONDENACAO_INDIVIDUAL = 10;
-	private int PENA_CONDENACAO_CUMPLICES = 11;
+	private int PENA_CONDENACAO_CUMPLICES = 1;
 
 	public int calculaPena(String StringPrisioneiroA, String StringPrisioneiroB) {
 		if (StringPrisioneiroA.equalsIgnoreCase("Culpado")) {
 			if (StringPrisioneiroB.equalsIgnoreCase("Culpado")){
 				return PENA_CONDENACAO_MUTUA;
 			} else {
-				return PENA_INOCENCIA;
-			}
-		} else {
-			if (StringPrisioneiroB.equalsIgnoreCase("Culpado")){
 				return PENA_CONDENACAO_INDIVIDUAL;
-			} else {
+			}
+		}
+		else {
+			if (StringPrisioneiroB.equalsIgnoreCase("Culpado")){
 				return PENA_CONDENACAO_CUMPLICES;
+			} else {
+				return PENA_INOCENCIA;
 			}
 		}
 	}
--- ProjetoNac/src/JulgamentoPrisioneiroTest.java
+++ ProjetoNac/src/JulgamentoPrisioneiroTest.java
@@ -3,14 +3,34 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class JulgamentoPrisioneiroTest {
+	
+	JulgamentoPrisioneiro obj = new JulgamentoPrisioneiro();
 
 	@Test
-	void testCalculaPena() {
-		JulgamentoPrisioneiro obj = new JulgamentoPrisioneiro();
+	void testCalculaPenaMutua() {
 		int resultadoEsperado = 5;
 		int resultadoReal = obj.calculaPena("Culpado", "Culpado");
 		assertEquals(resultadoEsperado, resultadoReal);
-		
 	}
-
+	
+	@Test
+	void testCalculaInocencia() {
+		int resultadoEsperado = 0;
+		int resultadoReal = obj.calculaPena("Inocente", "Inocente");
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
+	@Test
+	void testCalculaPenaIndividual() {
+		int resultadoEsperado = 10;
+		int resultadoReal = obj.calculaPena("Culpado", "Inocente");
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
+	
+	@Test
+	void testCalculaPenaCumplices() {
+		int resultadoEsperado = 1;
+		int resultadoReal = obj.calculaPena("Inocente", "Culpado");
+		assertEquals(resultadoEsperado, resultadoReal);
+	}
 }
